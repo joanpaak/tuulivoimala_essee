31df4ae85c8e19be9f3eb44afa35684ae7a5c8be
joanpaak <64299822+joanpaak@users.noreply.github.com>
2020-04-25 09:30:52 +0300

    Update props_reporting_symptoms.R
    Clear the comments.

--- R/props_reporting_symptoms.R
+++ R/props_reporting_symptoms.R
@@ -1,10 +1,14 @@
 
+# Proportions of participants reporting symptoms during the experiment.
+# 6 out of 11, who reporting getting symptoms from (infra)sounds emitted from wind turbines, reported symptoms. 
+# 2 out of 16, who reported NOT getting symptoms from (infra)sounds emitted from wind turbines, reported symptoms. 
 #
+# Thus:
 # P1 = 6/11
 # P2 = 2/16
 #
-
 # Prior = Beta(1, 1)
+#
 
 # 1) Plot posterior distributions:
 
